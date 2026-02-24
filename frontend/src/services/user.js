e7eedeaa3065f107d9fbb465057ec6f8c9828eee
AnuragMadnaik18 <anuragmadnaik9381@gmail.com>
2026-02-24 14:59:54 +0530

    integrate update profile and change password APIs

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 import API_BASE_URL from "./config";
 
+// Get JWT from sessionStorage
+const getAuthHeader = () => {
+    const token = sessionStorage.getItem("token");
+    return {
+        headers : {
+            Authorization: `Bearer ${token}`,
+        },
+    }
+}
+
 //Register API
 export const registerUser = async(userData) => {
     return await axios.post(`${API_BASE_URL}/users/register`,userData);
@@ -11,4 +21,16 @@ export const loginUser = async(loginData) => {
     return await axios.post(`${API_BASE_URL}/users/login`,loginData);
 }
 
+// To update profile
+export const updateProfile = (id,data) => {
+    return axios.put(`${API_BASE_URL}/users/update-profile/${id}`,data,
+        getAuthHeader()
+    );
+}
 
+// to change password
+export const changePassword = (id,data) => {
+    return axios.put(`${API_BASE_URL}/users/change-password/${id}`,data,
+        getAuthHeader()
+    );
+}
