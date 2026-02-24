63bf2975e277425a01248a39f822b75f6d69d255
AnuragMadnaik18 <anuragmadnaik9381@gmail.com>
2026-02-24 14:57:56 +0530

    implement update profile and change password logic

--- backend/src/main/java/com/smarthire/service/UserService.java
+++ backend/src/main/java/com/smarthire/service/UserService.java
@@ -2,10 +2,14 @@ package com.smarthire.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.smarthire.dto.ChangePasswordRequestDto;
 import com.smarthire.dto.RegisterRequestDto;
+import com.smarthire.dto.UpdateProfileRequestDto;
 import com.smarthire.entity.User;
 
 public interface UserService extends UserDetailsService{
 	User registerUser(RegisterRequestDto dto);
 	User getUserById(Long id);
+	void updateProfile(Long userId, UpdateProfileRequestDto dto);
+	void changePassword(Long userId,ChangePasswordRequestDto dto);
 }
--- backend/src/main/java/com/smarthire/service/UserServiceImpl.java
+++ backend/src/main/java/com/smarthire/service/UserServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 import com.smarthire.custom_exceptions.EmailAlreadyExistsException;
 import com.smarthire.custom_exceptions.UserNotFoundException;
 import com.smarthire.dao.UserDao;
+import com.smarthire.dto.ChangePasswordRequestDto;
 import com.smarthire.dto.RegisterRequestDto;
+import com.smarthire.dto.UpdateProfileRequestDto;
 import com.smarthire.entity.User;
 import com.smarthire.enums.AccountStatus;
 import com.smarthire.enums.Role;
@@ -58,4 +60,28 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         return userDao.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
+
+	@Override
+	public void updateProfile(Long userId, UpdateProfileRequestDto dto) {
+		User user = userDao.findById(userId)
+				.orElseThrow(()->new UserNotFoundException("User not found"));
+		user.setFullName(dto.getFullName());
+		user.setEmail(dto.getEmail());
+		user.setPhoneNumber(dto.getPhoneNumber());
+		userDao.save(user);
+	}
+
+	@Override
+	public void changePassword(Long userId, ChangePasswordRequestDto dto) {
+		User user = userDao.findById(userId)
+				.orElseThrow(()-> new UserNotFoundException("User not found"));
+		if(!passwordEncoder.matches(dto.getCurrentPassword(), user.getPassword()))
+			throw new RuntimeException("Current pasword is incorrect");
+		user.setPassword(passwordEncoder.encode(dto.getNewPassword()));
+		System.out.println("Entered Current Password: " + dto.getCurrentPassword());
+		System.out.println("Stored Encoded Password: " + user.getPassword());
+		System.out.println("Password Match Result: " +
+		        passwordEncoder.matches(dto.getCurrentPassword(), user.getPassword()));
+		userDao.save(user);
+	}
 }
