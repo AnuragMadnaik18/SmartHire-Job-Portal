97ddc09fc4643e4f70e37befd2033d88fe4d9d9a
AnuragMadnaik18 <anuragmadnaik9381@gmail.com>
2026-02-24 15:03:54 +0530

    add ChangePasswordRequestDto and UpdateProfileRequestDto

---
+++ backend/src/main/java/com/smarthire/dto/ChangePasswordRequestDto.java
@@ -0,0 +1,13 @@
+package com.smarthire.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequestDto {
+	private String currentPassword;
+	private String newPassword;
+}
